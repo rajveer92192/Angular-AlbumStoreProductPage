fda730a5173a7d92cabd681b37bc14e9166c4eb6
Rajveer Singh <rajveer.singh@nagarro.com>
2019-06-03 18:51:56 +0530

    Completed the module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'app/product.service';
 
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
