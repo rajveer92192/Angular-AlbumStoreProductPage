05d861656f1da82471c9a0e306c1306d6764b368
Rajveer Singh <rajveer.singh@nagarro.com>
2019-06-04 15:30:49 +0530

    Completed the module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
-import { Album } from '../album';
+import { Album } from 'app/album';
 
 
 @Component({
